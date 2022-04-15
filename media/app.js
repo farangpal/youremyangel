4a5ee1b0310087f3c10e985b3541f606e640c881
farangpal <98533096+farangpal@users.noreply.github.com>
2022-04-15 10:32:18 +0000

    Update app.js

--- media/app.js
+++ media/app.js
@@ -42,8 +42,8 @@ function sendMessage(cont) {
 
 }
 
-let plusBtn = document.querySelector('button[class*="btn-plus"]');
-let minusBtn = document.querySelector('button[class*="btn-minus"]');
+let plusBtn = document.querySelector('button[id*="btn-plus"]');
+let minusBtn = document.querySelector('button[id*="btn-minus"]');
 let totalNFTInput = document.querySelector('input[type="number"][id="totalNFT"]')
 let totalETHSpan =  document.querySelector('#totalETH');
 totalNFTInput.value = 1;
